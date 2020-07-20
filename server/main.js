b2bce5c3297f6d10e2f5e2b5a56905dadb5c1e6a
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-07-20 11:11:19 -0600

    Agregando io connection , tambien agregando el static

--- server/main.js
+++ server/main.js
@@ -3,10 +3,16 @@ var app= express();
 var server= require('http').Server(app);
 var io= require('socket.io')(server);
 
+
+app.use(express.static('public'))
+
 app.get('/',function(req,res){
-    res.status(200).send("hola")
+    res.status(200).send("Hello World")
 })
 
+io.on('connection',function(socket){
+    console.log("Alguien esta tratando de comunicarse")
+})
 
 server.listen(8080,function(){
      console.log("Servidor Corrriendo en http://localhost:8080")
